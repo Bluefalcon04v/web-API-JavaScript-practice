e865762f59ee7272b6af43af6f2e6ee6329141a1
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-09-13 18:03:33 +0530

    fetching APIs with arrow function

---
+++ fetchWebAPIsUsingArrowFunction.js
@@ -0,0 +1,10 @@
+const url = "https://www.discoveryvip.com/shared/json.php?f=peopledata1"; 
+let output = document.querySelector(".container");
+
+fetch(url).then(res => res.json()).then(value => {
+    let i = 0;
+    value.data.forEach(names => {
+        output.innerHTML += `${i}) firstName: ${names.firstName} lastName: ${names.lastName} <br/>`
+        i += 1;
+    })
+})
